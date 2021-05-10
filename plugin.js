4427f2a292e40dac218beeef7286924cff49f683
Vladimir Kotikov <kotikov.vladimir@gmail.com>
2021-05-11 00:01:16 +0300

    Tweak MQTT part to match yandex2mqtt implementation

--- plugin.js
+++ plugin.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var AliceLocal = require('./lib/aliceLocal.js').aliceLocal;
+var setupMQTT = require('./lib/aliceLocal.js').setupMQTT;
 var aliceActions = require('./lib/aliceActions.js');
 var EventEmitter = require('events').EventEmitter;
 // var debug = require('debug')('alicePlugin');
@@ -9,17 +9,15 @@ const packageConfig = require('./package.json');
 
 var options = {};
 
-module.exports = function(homebridge) {
-  homebridge.registerPlatform("homebridge-g-on-alice", "G-On Alice", aliceHome);
+module.exports = function (homebridge) {
+  homebridge.registerPlatform("homebridge-alice2mqtt", aliceHome);
 };
 
 function aliceHome(log, config, api) {
   this.log = log;
   this.eventBus = new EventEmitter();
   this.config = config;
-  this.pin = config['pin'] || "031-45-154";
-  this.username = config['username'] || false;
-  this.password = config['password'] || false;
+  this.pin = config['pin']
 
   // Enable config based DEBUG logging enable
   this.debug = config['debug'] || false;
@@ -37,10 +35,6 @@ function aliceHome(log, config, api) {
     debugEnable.enable(namespaces);
   }
 
-  if (!this.username || !this.password) {
-    this.log.error("Missing username and password");
-  }
-
   if (api) {
     this.api = api;
     this.api.on('didFinishLaunching', this.didFinishLaunching.bind(this));
@@ -53,44 +47,33 @@ function aliceHome(log, config, api) {
 }
 
 aliceHome.prototype = {
-  accessories: function(callback) {
+  accessories: function (callback) {
     this.log("accessories");
     callback();
   }
 };
 
-aliceHome.prototype.didFinishLaunching = function() {
-  var host = 'homebridge.g-on.io';
-  // var host = 'localhost';
-
+aliceHome.prototype.didFinishLaunching = function () {
   options = {
     eventBus: this.eventBus,
-    username: this.username,
-    password: this.password,
-    clientId: this.username,
     debug: this.debug,
     log: this.log,
     pin: this.pin,
-    servers: [{
-      protocol: 'mqtt',
-      host: host,
-      port: 1883
-    }]
   };
 
   // Initialize HAP Connections
   aliceActions.hapDiscovery(options);
 
-  var alice = new AliceLocal(options);
+  setupMQTT(this.config.brokerUrl, options);
 
   // Alice mesages
 
-  this.eventBus.on('discovery', aliceActions.aliceDiscovery.bind(this));
+  this.eventBus.on('devices', aliceActions.aliceDiscovery.bind(this));
   this.eventBus.on('action', aliceActions.aliceAction.bind(this));
   this.eventBus.on('query', aliceActions.aliceQuery.bind(this));
 };
 
-aliceHome.prototype.configureAccessory = function(accessory) {
+aliceHome.prototype.configureAccessory = function (accessory) {
   this.log("configureAccessory");
   // callback();
 };
